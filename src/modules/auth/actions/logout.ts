d4463c9c8d743f39546c316903711ac216be7942
Khoa Tran <khoa.trandang30620@gmail.com>
2025-02-14 17:08:31 +0700

    #4 feat: add logout feat

---
+++ src/modules/auth/actions/logout.ts
@@ -0,0 +1,37 @@
+'use server'
+
+import { redirect } from 'next/navigation'
+
+import { auth, signOut } from '@/auth'
+import { ErrorCode } from '@/constraint/code'
+import { http } from '@/lib/http'
+
+export const logout = async () => {
+	const session = await auth()
+
+	if (!session) {
+		redirect('/auth/login')
+	}
+
+	console.log(session.user)
+
+	const res = await http.put(`/auth/sign-out/${session.user.id}`)
+
+	const resData = res.data
+
+	if (res.status === 200) {
+		await signOut()
+
+		return {
+			success: true,
+			statusCode: resData.statusCode,
+			message: 'Đăng xuất thành công'
+		}
+	}
+
+	return {
+		success: false,
+		statusCode: ErrorCode.WRONG_CREDENTIALS_ERROR,
+		message: 'Đăng xuất thất bại, hãy thử lại sau'
+	}
+}
