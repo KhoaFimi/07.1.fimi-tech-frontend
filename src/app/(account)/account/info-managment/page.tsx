d3cb49fbee967a6d749285082441d39f89c5fc26
Khoa Tran <khoa.trandang30620@gmail.com>
2025-02-14 16:18:04 +0700

    #4 fix:add suspen, fix eslint

---
+++ src/app/(account)/account/info-managment/page.tsx
@@ -0,0 +1,5 @@
+const ManagementInfoPage = () => {
+	return <div>ManagementInfo</div>
+}
+
+export default ManagementInfoPage
--- src/app/(account)/account/reset-password/page.tsx
+++ src/app/(account)/account/reset-password/page.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from 'react'
+
 import ResetPasswordForm from '@/modules/account/components/form/reset-password-form'
 
 const ResetPasswordPage = () => {
-	return <ResetPasswordForm />
+	return (
+		<Suspense>
+			<ResetPasswordForm />
+		</Suspense>
+	)
 }
 
 export default ResetPasswordPage
