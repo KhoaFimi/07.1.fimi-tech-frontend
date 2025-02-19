68e92874a26353b24d5fa5e028a1d77ae88b3194
Khoa Tran <khoa.trandang30620@gmail.com>
2025-02-19 12:03:20 +0700

    #0 fix:

--- src/auth.ts
+++ src/auth.ts
@@ -61,6 +61,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 			return refreshAccessToken(token)
 		},
 		session: async ({ session, token }) => {
+			console.log('In session: ', {
+				token,
+				session
+			})
+
 			if (token) {
 				session.userId = token.sub as string
 				session.user.id = token.sub as string
